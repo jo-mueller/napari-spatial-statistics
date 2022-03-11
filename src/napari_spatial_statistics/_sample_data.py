5f5789a58619aa57714a564cee1d8e1f094dbee7
Johannes Müller <johannes.mueller88@hotmail.de>
2022-03-11 16:37:11 +0100

    added more options for random sample data

--- src/napari_spatial_statistics/_sample_data.py
+++ src/napari_spatial_statistics/_sample_data.py
@@ -8,10 +8,23 @@ Replace code below according to your needs.
 """
 import numpy as np
 
-from napari.types import PointsData, LayerDataTuple
+from napari.types import LayerDataTuple, List
 
-def make_random_points(n_points=40,
-                       size:int = 1000,
-                       dim: int = 3) -> PointsData:
-  data = size * np.random.random((n_points, dim))
-  return data
+def make_random_points(n_points=100,
+                       size:int = 100,
+                       dim: int = 3,
+                       number_of_layers:int = 2) -> List[LayerDataTuple]:
+
+    colors = ['orange', 'blue', 'magenta', 'pink', 'red', 'green', 'yellow']
+
+    data = [
+        (size * np.random.random((n_points, dim)),
+        {
+            'name': f'Random Points_{idx}',
+            'size': size/40,
+            'edge_width': 0,
+            'face_color': colors[idx % len(colors)]
+         },
+        'points')
+        for idx in range(number_of_layers)]
+    return data
