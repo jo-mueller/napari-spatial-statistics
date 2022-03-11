51c6eb5a8f289dafcccfce8143685c4aa1ba13e2
Johannes Müller <johannes.mueller88@hotmail.de>
2022-03-11 23:19:57 +0100

    added test for new generator

--- src/napari_spatial_statistics/_tests/test_sample_data.py
+++ src/napari_spatial_statistics/_tests/test_sample_data.py
@@ -1,4 +1,4 @@
-from napari_spatial_statistics import make_random_points
+from napari_spatial_statistics import make_random_points, make_2ch_non_random_points
 import numpy as np
 
 
@@ -13,3 +13,13 @@ def test_point_generation(make_napari_viewer):
     assert len(viewer.layers) == 1
 
     assert len(np.unique(pts[1]['properties']['Cell type'])) == n_classes
+
+def test_correlated_data_generator(make_napari_viewer):
+
+    viewer = make_napari_viewer()
+
+    pts = make_2ch_non_random_points()
+    viewer.add_points(pts[0], **pts[1])
+
+    assert len(viewer.layers) == 1
+    assert len(np.unique(pts[1]['properties']['Cell type'])) == 2
