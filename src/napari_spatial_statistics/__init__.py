1d47dfbd95e4cda10836de738898aa31bf8d1ad7
Johannes Müller <johannes.mueller88@hotmail.de>
2022-03-14 16:04:59 +0100

    changed and added function names. Removed main from file

--- src/napari_spatial_statistics/__init__.py
+++ src/napari_spatial_statistics/__init__.py
@@ -2,26 +2,22 @@
 __version__ = "0.0.1"
 
 from napari_spatial_statistics._sample_data import make_random_points, make_2ch_non_random_points
-from napari_spatial_statistics._spatial_statistics import neighborhood_enrichment_test
+# from napari_spatial_statistics._spatial_statistics import spatial_statistics
 from napari_spatial_statistics._plot_widget import PlotWidget
+
+from napari_spatial_statistics._neighborhood import distance_ckdtree, \
+    distance_squidpy,\
+    knearest_ckdtree
 
 from napari_plugin_engine import napari_hook_implementation
 
 
 @napari_hook_implementation
 def napari_experimental_provide_function():
-    return [neighborhood_enrichment_test,
+    return [spatial_statistics,
             make_random_points,
             make_2ch_non_random_points,
-            PlotWidget]
-
-
-if __name__ == '__main__':
-
-    import napari
-    viewer = napari.Viewer()
-
-    pts = make_2ch_non_random_points()
-    viewer.add_points(pts[0], **pts[1])
-
-    neighborhood_enrichment_test(viewer, pts)
+            PlotWidget,
+            distance_ckdtree,
+            distance_squidpy,
+            knearest_ckdtree]
