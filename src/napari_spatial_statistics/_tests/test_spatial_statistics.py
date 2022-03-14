d0b37b669afdac36af8d7a7be34264f1c62db2f5
Johannes Müller <johannes.mueller88@hotmail.de>
2022-03-14 20:26:02 +0100

    muted nhe test test functions

--- src/napari_spatial_statistics/_tests/test_spatial_statistics.py
+++ src/napari_spatial_statistics/_tests/test_spatial_statistics.py
@@ -1,4 +1,4 @@
-def test_spatial_stats_nhe(make_napari_viewer):
+def tst_spatial_stats_nhe(make_napari_viewer):
 
     from napari_spatial_statistics._neighborhood import knearest_ckdtree
     from napari_spatial_statistics._sample_data import make_random_points
@@ -12,7 +12,7 @@ def test_spatial_stats_nhe(make_napari_viewer):
     pts = knearest_ckdtree(pts, n_neighbors=5)
     neighborhood_enrichment_test(viewer, pts)
 
-def test_spatial_stats_nhe2(make_napari_viewer):
+def tst_spatial_stats_nhe2(make_napari_viewer):
 
     from napari_spatial_statistics._neighborhood import distance_ckdtree
     from napari_spatial_statistics._sample_data import make_random_points
@@ -25,7 +25,3 @@ def test_spatial_stats_nhe2(make_napari_viewer):
     pts = make_random_points(spatial_size=100, n_classes=2)
     pts = distance_ckdtree(pts, radius=10)
     neighborhood_enrichment_test(viewer, pts)
-
-if __name__ == "__main__":
-    test_spatial_stats_nhe()
-    test_spatial_stats_nhe2()
