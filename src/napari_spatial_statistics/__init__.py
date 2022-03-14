2e96224bdbb953a5cc6ef4a7314429199d36d68a
Johannes Müller <johannes.mueller88@hotmail.de>
2022-03-14 16:39:07 +0100

    Changed name in init

--- src/napari_spatial_statistics/__init__.py
+++ src/napari_spatial_statistics/__init__.py
@@ -2,7 +2,7 @@
 __version__ = "0.0.1"
 
 from napari_spatial_statistics._sample_data import make_random_points, make_2ch_non_random_points
-# from napari_spatial_statistics._spatial_statistics import spatial_statistics
+from napari_spatial_statistics._spatial_statistics import neighborhood_enrichment_test
 from napari_spatial_statistics._plot_widget import PlotWidget
 
 from napari_spatial_statistics._neighborhood import distance_ckdtree, \
@@ -14,7 +14,7 @@ from napari_plugin_engine import napari_hook_implementation
 
 @napari_hook_implementation
 def napari_experimental_provide_function():
-    return [spatial_statistics,
+    return [neighborhood_enrichment_test,
             make_random_points,
             make_2ch_non_random_points,
             PlotWidget,
