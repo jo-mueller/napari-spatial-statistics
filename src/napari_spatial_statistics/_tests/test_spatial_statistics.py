de97d38128b5fecb84873ad1b307f538ddc90efc
Johannes Müller <johannes.mueller88@hotmail.de>
2022-03-14 17:43:13 +0100

    Update test_spatial_statistics.py

--- src/napari_spatial_statistics/_tests/test_spatial_statistics.py
+++ src/napari_spatial_statistics/_tests/test_spatial_statistics.py
@@ -1,30 +1,30 @@
-def test_spatial_stats_nhe():
+def test_spatial_stats_nhe(make_napari_viewer):
 
     from napari_spatial_statistics._neighborhood import knearest_ckdtree
     from napari_spatial_statistics._sample_data import make_random_points
     from napari_spatial_statistics._spatial_statistics import neighborhood_enrichment_test
 
-    # viewer = make_napari_viewer()
-    import napari
-    viewer = napari.Viewer()
+    viewer = make_napari_viewer()
+    # import napari
+    # viewer = napari.Viewer()
 
     pts = make_random_points(spatial_size=100, n_classes=2)
-    pts1 = knearest_ckdtree(pts, n_neighbors=5)
-    neighborhood_enrichment_test(viewer, pts1)
+    pts = knearest_ckdtree(pts, n_neighbors=5)
+    neighborhood_enrichment_test(viewer, pts)
 
-def test_spatial_stats_nhe2():
+def test_spatial_stats_nhe2(make_napari_viewer):
 
     from napari_spatial_statistics._neighborhood import distance_ckdtree
     from napari_spatial_statistics._sample_data import make_random_points
     from napari_spatial_statistics._spatial_statistics import neighborhood_enrichment_test
 
-    # viewer = make_napari_viewer()
-    import napari
-    viewer = napari.Viewer()
+    viewer = make_napari_viewer()
+    # import napari
+    # viewer = napari.Viewer()
 
     pts = make_random_points(spatial_size=100, n_classes=2)
-    pts1 = distance_ckdtree(pts, radius=10)
-    neighborhood_enrichment_test(viewer, pts1)
+    pts = distance_ckdtree(pts, radius=10)
+    neighborhood_enrichment_test(viewer, pts)
 
 if __name__ == "__main__":
     test_spatial_stats_nhe()
