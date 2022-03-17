7ff065e1bb36483009b76e876379a4b037e74506
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-03-17 18:27:24 +0100

    fixed test
    (points need to be retrieved from viewer to have neighbors  property

--- src/napari_spatial_statistics/_tests/test_spatial_statistics.py
+++ src/napari_spatial_statistics/_tests/test_spatial_statistics.py
@@ -24,7 +24,13 @@ def test_spatial_stats_nhe2(make_napari_viewer):
     pts = make_random_points(spatial_size=100, n_classes=n_classes)
     pts = viewer.add_points(pts[0], **pts[1])
     distance_ckdtree(pts, radius=10, show_neighborhood=False)
+
+    pts = viewer.layers[0]
     result = neighborhood_enrichment_test(pts.data, pts.properties, on_feature='Cell type')
 
     assert 'zscore' in list(result.keys()) and 'count' in list(result.keys())
     assert np.array_equal(result['zscore'].shape, np.array([n_classes, n_classes]))
+
+if __name__ == '__main__':
+    import napari
+    test_spatial_stats_nhe2(napari.Viewer)
