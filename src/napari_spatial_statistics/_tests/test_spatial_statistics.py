1278ab356b19fce1d7f393f29878d1a8eb024b7e
Johannes Müller <johannes.mueller88@hotmail.de>
2022-03-17 16:38:29 +0100

    used fixture for test

--- src/napari_spatial_statistics/_tests/test_spatial_statistics.py
+++ src/napari_spatial_statistics/_tests/test_spatial_statistics.py
@@ -12,16 +12,23 @@ def tst_spatial_stats_nhe(make_napari_viewer):
     pts = knearest_ckdtree(pts, n_neighbors=5)
     neighborhood_enrichment_test(viewer, pts)
 
-def tst_spatial_stats_nhe2(make_napari_viewer):
+def tst_spatial_stats_nhe2():
 
     from napari_spatial_statistics._neighborhood import distance_ckdtree
     from napari_spatial_statistics._sample_data import make_random_points
     from napari_spatial_statistics._spatial_statistics import neighborhood_enrichment_test
+    from napari.layers import Points
 
-    viewer = make_napari_viewer()
-    # import napari
-    # viewer = napari.Viewer()
+    # viewer = make_napari_viewer()
+    import napari
+    viewer = napari.Viewer()
 
     pts = make_random_points(spatial_size=100, n_classes=2)
-    pts = distance_ckdtree(pts, radius=10)
-    neighborhood_enrichment_test(viewer, pts)
+    pts = viewer.add_points(pts[0], **pts[1])
+    distance_ckdtree(pts, radius=10, show_neighborhood=False)
+
+
+    wdg = neighborhood_enrichment_test()
+
+if __name__ == "__main__":
+    tst_spatial_stats_nhe2()
