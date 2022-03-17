13b694f0fd8069da66c064061d00cd91150fe653
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-03-17 17:44:49 +0100

    used ficture and added test for nhetest output

--- src/napari_spatial_statistics/_tests/test_spatial_statistics.py
+++ src/napari_spatial_statistics/_tests/test_spatial_statistics.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def tst_spatial_stats_nhe(make_napari_viewer):
 
     from napari_spatial_statistics._neighborhood import knearest_ckdtree
@@ -5,30 +7,24 @@ def tst_spatial_stats_nhe(make_napari_viewer):
     from napari_spatial_statistics._spatial_statistics import neighborhood_enrichment_test
 
     viewer = make_napari_viewer()
-    # import napari
-    # viewer = napari.Viewer()
 
     pts = make_random_points(spatial_size=100, n_classes=2)
     pts = knearest_ckdtree(pts, n_neighbors=5)
     neighborhood_enrichment_test(viewer, pts)
 
-def tst_spatial_stats_nhe2():
+def test_spatial_stats_nhe2(make_napari_viewer):
 
     from napari_spatial_statistics._neighborhood import distance_ckdtree
     from napari_spatial_statistics._sample_data import make_random_points
     from napari_spatial_statistics._spatial_statistics import neighborhood_enrichment_test
-    from napari.layers import Points
 
-    # viewer = make_napari_viewer()
-    import napari
-    viewer = napari.Viewer()
+    viewer = make_napari_viewer()
 
-    pts = make_random_points(spatial_size=100, n_classes=2)
+    n_classes = 2
+    pts = make_random_points(spatial_size=100, n_classes=n_classes)
     pts = viewer.add_points(pts[0], **pts[1])
     distance_ckdtree(pts, radius=10, show_neighborhood=False)
+    result = neighborhood_enrichment_test(pts.data, pts.properties, on_feature='Cell type')
 
-
-    wdg = neighborhood_enrichment_test()
-
-if __name__ == "__main__":
-    tst_spatial_stats_nhe2()
+    assert 'zscore' in list(result.keys()) and 'count' in list(result.keys())
+    assert np.array_equal(result['zscore'].shape, np.array([n_classes, n_classes]))
