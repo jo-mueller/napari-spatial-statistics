2bcd9511a2274cb38c4039268c7aa8aae3e4ee84
Johannes Müller <johannes.mueller88@hotmail.de>
2022-03-17 16:38:14 +0100

    added function to init

--- src/napari_spatial_statistics/__init__.py
+++ src/napari_spatial_statistics/__init__.py
@@ -2,23 +2,23 @@
 __version__ = "0.0.1"
 
 from ._sample_data import make_random_points, make_2ch_non_random_points
-from ._spatial_statistics import neighborhood_enrichment_test
+from ._spatial_statistics import nhe_test_widget
 from ._plot_widget import PlotWidget
 
 from ._neighborhood import distance_ckdtree, distance_squidpy,\
     knearest_ckdtree
 
-from ._show_neighbors import properties_to_table
-
 from napari_plugin_engine import napari_hook_implementation
-
 
 @napari_hook_implementation
 def napari_experimental_provide_function():
-    return [neighborhood_enrichment_test,
-            make_random_points,
+    return [make_random_points,
             make_2ch_non_random_points,
             PlotWidget,
             distance_ckdtree,
             distance_squidpy,
             knearest_ckdtree]
+
+@napari_hook_implementation
+def napari_experimental_provide_dock_widget():
+    return [nhe_test_widget]
