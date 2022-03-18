0f7a21a939773d5589a1ed3df472915d5d9c6778
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-03-18 17:23:02 +0100

    Change function name of non-random data

--- src/napari_spatial_statistics/__init__.py
+++ src/napari_spatial_statistics/__init__.py
@@ -1,7 +1,7 @@
 
 __version__ = "0.0.1"
 
-from ._sample_data import make_random_points, make_2ch_non_random_points
+from ._sample_data import make_random_points, make_non_random_points
 from ._spatial_statistics import nhe_test_widget
 from ._plot_widget import PlotWidget
 
@@ -13,7 +13,7 @@ from napari_plugin_engine import napari_hook_implementation
 @napari_hook_implementation
 def napari_experimental_provide_function():
     return [make_random_points,
-            make_2ch_non_random_points,
+            make_non_random_points,
             PlotWidget,
             distance_ckdtree,
             distance_squidpy,
