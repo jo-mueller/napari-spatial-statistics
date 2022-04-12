098e4e593f6b9165f7ac1f345a3924a7f809b8ee
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-04-12 09:43:51 +0200

    Added function to return sample data

--- src/napari_spatial_statistics/_sample_data.py
+++ src/napari_spatial_statistics/_sample_data.py
@@ -8,11 +8,22 @@ Replace code below according to your needs.
 """
 import numpy as np
 
-from napari.types import LayerDataTuple, List
+from napari.types import LayerDataTuple, List, ImageData
 
 
 img_colors = np.array(['bop orange', 'bop blue', 'magenta', 'pink', 'red', 'green', 'yellow'])
 pt_colors = np.array(['orange', 'blue', 'magenta', 'pink', 'red', 'green', 'yellow'])
+
+def brain_section() -> ImageData:
+    import os
+    import pathlib
+    from skimage import io
+
+    parent = pathlib.Path(__file__).parent.resolve()
+    filename = os.path.join(parent, '..', '..', 'docs', 'data', 'DAPI.png')
+    image = io.imread(filename)
+
+    return image
 
 def make_random_spots(n_spots: int = 1000,
                       n_classes: int = 2,
@@ -37,28 +48,37 @@ def make_random_spots(n_spots: int = 1000,
 
     return images
 
+# def make_random_points(n_points: int = 1000,
+#                         n_classes: int = 3,
+#                         spatial_size: int = 100,
+#                         dim: int = 3) -> List[LayerDataTuple]:
+
+#     data = spatial_size * np.random.random((n_points, dim))
+#     point_type = np.random.randint(0, n_classes, size=n_points)
+
+#     properties = {'Label': np.arange(0, n_points, 1), 'Cell type': point_type}
+#     props = {'name': 'Random points',
+#              'face_color': pt_colors[point_type],
+#              'edge_width': 0,
+#              'properties': properties,
+#              'size': spatial_size/40}
+
+#     return (data, props, 'points')
+
 def make_random_points(n_points: int = 1000,
-                        n_classes: int = 3,
-                        spatial_size: int = 100,
-                        dim: int = 3) -> List[LayerDataTuple]:
+                       n_classes: int = 3,
+                       spatial_size: int = 100,
+                       randomness: float = 2,
+                       dim: int = 3) -> List[LayerDataTuple]:
+    """
+    Create random points layer data.
 
-    data = spatial_size * np.random.random((n_points, dim))
-    point_type = np.random.randint(0, n_classes, size=n_points)
-
-    properties = {'Label': np.arange(0, n_points, 1), 'Cell type': point_type}
-    props = {'name': 'Random points',
-             'face_color': pt_colors[point_type],
-             'edge_width': 0,
-             'properties': properties,
-             'size': spatial_size/40}
-
-    return (data, props, 'points')
-
-def make_non_random_points(n_points: int = 1000,
-                           n_classes: int = 3,
-                           spatial_size: int = 100,
-                           sigma: float = 2,
-                           dim: int = 3) -> List[LayerDataTuple]:
+    This function will create a points layer with `n_classes` different object
+    types, which is stored in `layer.properties`. On creation, all point types
+    have the same position, making them highly spatially co-localized. A set
+    amount of noise can be added to the positions to make the object types'
+    locations more independent of each other.
+    """
 
     pts_per_channel = n_points//n_classes
     data_chx = []
@@ -69,7 +89,7 @@ def make_non_random_points(n_points: int = 1000,
                 spatial_size * np.random.random((pts_per_channel, dim))
                 )
         else:
-            noise = np.random.normal(scale=sigma, size=data_chx[0].shape)
+            noise = np.random.normal(scale=randomness, size=data_chx[0].shape)
             data_chx.append(
                 data_chx[idx-1] + noise
                 )
